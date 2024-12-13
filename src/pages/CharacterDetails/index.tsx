ca06e3eba8a7e4857b6e44c8f10dd6b29d797563
gabriel.chiode <CS428643@MinhaTI.com.br>
2024-12-13 16:36:44 -0300

    melhorando tela de detalhes do personagem

--- src/pages/CharacterDetails/index.tsx
+++ src/pages/CharacterDetails/index.tsx
@@ -5,11 +5,16 @@ import {
   CircularProgress,
   Alert,
   Card,
+  Avatar,
   CardContent,
-  CardMedia,
+  Grid2,
 } from '@mui/material';
+import InfoIcon from '@mui/icons-material/Info';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import PersonIcon from '@mui/icons-material/Person';
+import PetsIcon from '@mui/icons-material/Pets';
+import WcIcon from '@mui/icons-material/Wc';
 import useCharacterDetails from '../../api/hooks/useCharacterDetails';
-import AppBarCustom from '../../components/AppBar';
 
 function CharacterDetails() {
   const { id = '' } = useParams<{ id: string }>();
@@ -42,48 +47,107 @@ function CharacterDetails() {
   }
 
   return (
-    <>
-      <AppBarCustom />
-      <Box
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        height="100vh"
-      >
-        {character ? (
-          <Card sx={{ maxWidth: 400 }}>
-            <CardMedia
-              component="img"
-              height="300"
-              image={character.image}
-              alt={character.name}
-            />
-            <CardContent>
-              <Typography variant="h4" gutterBottom>
-                {character.name}
-              </Typography>
-              <Typography variant="body1">
-                Status: {character.status}
-              </Typography>
-              <Typography variant="body1">
-                Espécie: {character.species}
-              </Typography>
-              <Typography variant="body1">
-                Gênero: {character.gender}
-              </Typography>
-              <Typography variant="body1">
-                Origem: {character.origin?.name}
-              </Typography>
-              <Typography variant="body1">
-                Localização Atual: {character.location?.name}
-              </Typography>
-            </CardContent>
-          </Card>
-        ) : (
-          <Alert severity="info">Personagem não encontrado</Alert>
-        )}
-      </Box>
-    </>
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      height="100vh"
+    >
+      {character ? (
+        <Card
+          sx={{
+            display: 'flex',
+            maxWidth: 700,
+            p: 3,
+            borderRadius: 4,
+            background: 'linear-gradient(145deg, #1f1f1f, #272727)',
+            boxShadow: '0px 0px 15px 2px rgba(0, 255, 170, 0.7)',
+            border: '1px solid rgba(0, 255, 170, 0.4)',
+            overflow: 'hidden',
+          }}
+        >
+          {/* Avatar do personagem */}
+          <Avatar
+            src={character.image}
+            alt={character.name}
+            sx={{
+              width: 150,
+              height: 150,
+              margin: 'auto',
+              marginRight: 2,
+              border: '2px solid #ccc',
+              boxShadow: 2,
+            }}
+          />
+          {/* Informações */}
+          <CardContent>
+            <Typography
+              variant="h5"
+              fontWeight="bold"
+              gutterBottom
+              sx={{
+                color: '#00ffaa',
+                textShadow: '0 0 8px #00ffaa, 0 0 15px #00ffaa',
+              }}
+            >
+              {character.name}
+            </Typography>
+            <Grid2 container spacing={1} alignItems="center">
+              <Grid2>
+                <PetsIcon color="primary" />
+              </Grid2>
+              <Grid2>
+                <Typography variant="body1">
+                  Status: {character.status}
+                </Typography>
+              </Grid2>
+            </Grid2>
+            <Grid2 container spacing={1} alignItems="center">
+              <Grid2>
+                <PersonIcon color="primary" />
+              </Grid2>
+              <Grid2>
+                <Typography variant="body1">
+                  Espécie: {character.species}
+                </Typography>
+              </Grid2>
+            </Grid2>
+            <Grid2 container spacing={1} alignItems="center">
+              <Grid2>
+                <WcIcon color="primary" />
+              </Grid2>
+              <Grid2>
+                <Typography variant="body1">
+                  Gênero: {character.gender}
+                </Typography>
+              </Grid2>
+            </Grid2>
+            <Grid2 container spacing={1} alignItems="center">
+              <Grid2>
+                <LocationOnIcon color="primary" />
+              </Grid2>
+              <Grid2>
+                <Typography variant="body1">
+                  Origem: {character.origin?.name}
+                </Typography>
+              </Grid2>
+            </Grid2>
+            <Grid2 container spacing={1} alignItems="center">
+              <Grid2>
+                <InfoIcon color="primary" />
+              </Grid2>
+              <Grid2>
+                <Typography variant="body1">
+                  Localização Atual: {character.location?.name}
+                </Typography>
+              </Grid2>
+            </Grid2>
+          </CardContent>
+        </Card>
+      ) : (
+        <Alert severity="info">Personagem não encontrado</Alert>
+      )}
+    </Box>
   );
 }
 
